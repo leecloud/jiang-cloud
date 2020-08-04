aa47190566d9bd3e974725c9eac45c0cd3ecd8ae
liyunjiang <18083621534@163.com>
2020-08-04 16:45:16 +0800

    配置中心和监控中心
    配置中心和监控中心

---
+++ cloud-monitor/src/main/java/com/cloud/cloudmonitor/CloudMonitorApplication.java
@@ -0,0 +1,15 @@
+package com.cloud.cloudmonitor;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAdminServer
+public class CloudMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudMonitorApplication.class, args);
+    }
+
+}
---
+++ cloud-monitor/src/test/java/com/cloud/cloudmonitor/CloudMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.cloudmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
