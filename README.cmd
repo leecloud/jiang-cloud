ca64f18f9c6d7fd939e027b0454b5faf27fe32ad
leecloud <49024873+leecloud@users.noreply.github.com>
2020-08-07 09:21:49 +0800

    Update README.cmd

--- README.cmd
+++ README.cmd
@@ -1,1 +1,7 @@
 jiang-cloud是基于springcloud搭建的微服务后台
+1.eureka-server 是注册中心
+2.cloud-config 是配置中心
+3.cloud-monitor是基于springboot-admin的监控中心
+4.jiang-common是公共模块
+5.jiang-service是系统管理模块
+6.jiang-service-api是feigin远程调用模块
